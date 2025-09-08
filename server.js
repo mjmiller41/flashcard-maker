9d6d11e80838c15cafeb884eb8ad12d4ce62972a
Michael Joseph Miller <michael.joseph.miller@outlook.com>
2025-09-08 11:18:30 -0400

    fixed body-parser error, added .env support

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { GoogleGenAI } = require("@google/genai");
+require("dotenv").config();
+
 const app = express();
 const port = 3000;
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.static("public"));
 
 const genAI = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
@@ -17,7 +20,6 @@ app.post("/generate-flashcards-auto", async (req, res) => {
       contents: `Generate flashcards from the following text. Each flashcard should be in the format "question:answer". Only return the flashcards and nothing else. Do not include any introductory text or titles. For example:\nWhat is the capital of France?:Paris\n\nText: "${text}"`,
     });
     const flashcards = response.text;
-    console.log(flashcards);
     res.json({ flashcards });
   } catch (error) {
     console.error(error);
